17dfa544576fb84cb786ef84b2cee3471de03fa1
amvtek <devel@amvtek.com>
2014-09-26 19:37:03 +0300

    Modified thumbnailer to ease defining a {% thumbnail %}tag...

---
+++ djam/templatetags/djam.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+    djam.templatetags.djam
+    ~~~~~~~~~~~~~~~~~~~~~~
+
+    :copyright: (c) 2014 by sc AmvTek srl
+    :email: devel@amvtek.com
+"""
+
+from django import template
+from django.core.urlresolvers import reverse
+
+from ..thumbnailer import build_thumbnail_path
+
+register = template.Library()
+
+@register.simple_tag(takes_context=True)
+def thumbnail(context, mpath, width, height=None):
+    "return thumbnail url for mpath & dimensions..."
+
+    # retrieve thumbnailer_view name from context...
+    thumbnailer_view = context.get("thumbnailer_view", "thumbnailer")
+
+    # calculates thumbnail path
+    thumbpath = build_thumbnail_path(mpath, width, height)
+
+    return reverse(thumbnailer_view, args=(thumbpath,))
+
+
+
