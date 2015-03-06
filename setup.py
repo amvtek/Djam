7504e691e22c85c859a1f2b486e910f66d03e4c4
amvtek <devel@amvtek.com>
2015-03-06 11:58:30 +0200

    corrected bug in project setup.py which prevented package to be correctly installated

--- setup.py
+++ setup.py
@@ -7,7 +7,12 @@ setup(
     version='0.8.2',
     author='AmvTek developers',
     author_email='devel@amvtek.com',
-    packages=['djam'],
+    packages=[
+        'djam',
+        'djam.templatetags',
+        'djam.management',
+        'djam.management.commands'
+        ],
     url='https://github.com/amvtek/Djam/',
     license='MIT',
     description='Extends Django to work with sqlalchemy and make it behave like Flask',
