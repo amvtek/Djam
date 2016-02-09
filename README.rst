958e0eec6f9f02d7aeedeb32064252fbb9098c59
amvtek <devel@amvtek.com>
2016-02-09 12:38:58 +0200

    Improved project setup installer to make use of setuptools

--- README.rst
+++ README.rst
@@ -10,8 +10,10 @@ extends Django in various way :
 3. global request object
 4. class based view that works like middleware stack  
 5. email templates
+6. middleware that allows fine grained control over `CORS`_ authorizations
 
 Some of those components have been inspired by the `Flask`_ and `WTForms`_ frameworks.
 
 .. _Flask: http://flask.pocoo.org/
 .. _WTForms: https://wtforms.readthedocs.org/en/latest/
+.. _CORS: https://www.w3.org/TR/cors/
--- setup.py
+++ setup.py
@@ -1,18 +1,13 @@
 # -*- coding: utf-8 -*-
 
-from distutils.core import setup
+from setuptools import setup, find_packages
 
 setup(
     name='Djam',
-    version='0.8.3',
+    version='0.9.0.dev0',
     author='AmvTek developers',
     author_email='devel@amvtek.com',
-    packages=[
-        'djam',
-        'djam.templatetags',
-        'djam.management',
-        'djam.management.commands'
-        ],
+    packages=find_packages(),
     url='https://github.com/amvtek/Djam/',
     license='MIT',
     description='Extends Django to work with sqlalchemy and make it behave like Flask',
