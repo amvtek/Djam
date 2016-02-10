ed943dbe4235754ba8321746c66b8e588fb521ad
amvtek <devel@amvtek.com>
2016-02-10 09:37:34 +0200

    Fixing other PyPI hurdle : licence --> license

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     version='0.9.1',
     author='AmvTek developers',
     author_email='devel@amvtek.com',
-    licence='MIT',
+    license='MIT',
     packages=find_packages(),
     url='https://github.com/amvtek/Djam/',
     description='Extends Django to work with sqlalchemy and make it behave like Flask',
