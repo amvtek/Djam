a907463b37b1740f5c28154c0613966215f67cf9
amvtek <devel@amvtek.com>
2016-04-19 10:08:45 +0300

    Bumping version number to 0.9.2

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='Djam',
-    version='0.9.1',
+    version='0.9.2',
     author='AmvTek developers',
     author_email='devel@amvtek.com',
     license='MIT',
